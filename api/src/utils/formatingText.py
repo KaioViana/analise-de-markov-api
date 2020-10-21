de31db9b5849b8e733cee013b7664287faa2f0a6
Drasi Estratégia e Processos <60388645+drasi-dev@users.noreply.github.com>
2020-10-21 07:48:27 -0300

    formatação de texto atualizada

--- api/src/utils/formatingText.py
+++ api/src/utils/formatingText.py
@@ -1,9 +1,10 @@
 def formatText(text):
     import re
+    
 
     text = text.strip()
     text = text.replace('\n', '')
-    text = re.sub(r'(http\S*|@\S*|(-\s*)|;)', '', text)
+    text = re.sub(r'(http\S*|@\S*|(-\s*)|;|#\S*)', '', text)
     text = re.sub(
                     r'[(\U0001F600-\U0001F92F|\U0001F300-\U0001F5FF|\U0001F680-\U0001F6FF|\U0001F190-\U0001F1FF|\U00002702-\U000027B0|\U0001F926-\U0001FA9F|\u200d|\u2640-\u2642|\u2600-\u2B55|\u23cf|\u23e9|\u231a|\ufe0f)]*',
                     '',
