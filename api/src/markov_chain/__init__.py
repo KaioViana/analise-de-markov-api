f78c9a03685ea57c046a579d9e93737a3c9fbafd
Kaio César Viana <kaiocesarviana@localhost.localdomain>
2020-11-16 23:16:54 -0300

    algoritmo atualizado. falta tratativas e melhorias, nao finalizado.

--- api/src/markov_chain/__init__.py
+++ api/src/markov_chain/__init__.py
@@ -1,49 +1,82 @@
-from numpy import random
-
-
 class MarkovChain:
-    # def chain(self, text, n=30):
-    #     corpus = text.split()
-    #     pairs = self.__make_pairs(corpus)
-    #     word_dict = dict()
+    def __init__(self, corpus):
+        self.corpus_words, self.distinct_words, self.word_idx_dict = self.pre_processing(corpus)
 
 
-    #     for word_1, word_2 in pairs:
-    #         word_dict.setdefault(word_1, []).append(word_2)
-            
-    #     first_word = random.choice(corpus)
-
-    #     while first_word.islower():
-    #         first_word = random.choice(corpus)
-        
-    #     chain = [first_word]
-
-    #     for i in range(n):
-    #         chain.append(random.choice(word_dict[chain[-1]]))
-
-    #     return chain
-
-
-    # def __make_pairs(self, corpus):
-    #     for i in range(len(corpus)-1):
-    #         yield (corpus[i], corpus[i+1])
-
+    
     def pre_processing(self, corpus):
+        # adicionando espaços entre cada token
         for spaced in ['.','-',',','!','?','(','—',')''.','-',',','!','?','(','—',')']:
             corpus = corpus.replace(spaced, ' {0} '.format(spaced))
-        corpus_words = corpus.split()
-        corpus_words = [word for word in corpus_words if word != '']
 
-        distinct_words = list(set(corpus_words))
+        corpus_words = corpus.split() # lista de palavrask_words_idx_dict
+        corpus_words = [word for word in corpus_words if word != ''] # limpando lista
+        distinct_words = list(set(corpus_words)) # lista de palavras distintas
+        word_idx_dict = {word: i for i, word in enumerate(distinct_words)}
 
-        return corpus_words, distinct_words
+        return corpus_words, distinct_words, word_idx_dict # tupla
     
-    def train(self, corpus_words, distinct_words, k=2):
+    def train(self, k=1):
         from scipy.sparse import dok_matrix # para criação de matrizes esparsas
 
 
-        sets_of_k_words = [' '.join(corpus_words[i:i+k]) for i, _ in enumerate(corpus_words[:-k])] # montando o conjunto de palavras
+        sets_of_k_words = [' '.join(self.corpus_words[i:i+k]) for i, _ in enumerate(self.corpus_words)] # montando o conjunto de palavras
+        print(sets_of_k_words)
         sets_count = len(list(set(sets_of_k_words))) # quantidade de valores únicos no conjunto
+        distinct_sets_of_k_words = list(set(sets_of_k_words)) # lista de palavras distintas no conjunto
+        k_words_idx_dict = {word: i for i, word in enumerate(distinct_sets_of_k_words)} 
 
-        next_after_k_words_matrix = dok_matrix((sets_count, len(distinct_words))) # montando a matriz
+        next_after_k_words_matrix = dok_matrix((sets_count, len(self.distinct_words))) # montando a matriz
 
+        # preenchendo a matriz
+        for i, word in enumerate(sets_of_k_words[:-k]):
+            word_sequence_idx = k_words_idx_dict[word]
+            next_word_idx = self.word_idx_dict[self.corpus_words[i+k]]
+            next_after_k_words_matrix[word_sequence_idx, next_word_idx] += 1
+        
+        return next_after_k_words_matrix, k_words_idx_dict
+
+
+    def sample_next_word_after_sequence(self, word_sequence, next_after_k_words_matrix, k_words_idx_dict, alpha=0):
+        next_word_vector = next_after_k_words_matrix[k_words_idx_dict[word_sequence]]
+        likelihoods = next_word_vector/next_word_vector.sum()
+            
+        return self.weighted_choice(self.distinct_words, likelihoods.toarray())
+        
+
+    def stochastic_chain(self, seed, next_after_k_words_matrix, k_words_idx_dict, chain_length=15, seed_length=1):
+        current_words = seed.split(' ')
+
+        if len(current_words) != seed_length:
+            raise ValueError (f'número errado de palavras, é esperado {seed_length}')
+
+        sentence = seed
+
+        for _ in range(chain_length):
+            sentence += ' '
+            next_word = self.sample_next_word_after_sequence(' '.join(current_words), next_after_k_words_matrix, k_words_idx_dict, self.distinct_words)
+            print(next_word)
+            sentence += next_word
+            current_words = current_words[1:] + [next_word]
+            
+        return sentence
+
+
+    def weighted_choice(self, objects, weights):
+        """ retorna um elemento aleatório da sequencia de objetos
+        de acordo com seu peso (percentagem)"""
+
+        import numpy as np
+        import random
+        from random import random
+
+
+        weights = np.array(weights, dtype=np.float64)
+        sum_of_weights = weights.sum()
+
+        np.multiply(weights, 1/sum_of_weights, weights)
+        weights = weights.cumsum()
+        x = random()
+        for i in range(len(weights)):
+            if x < weights[i]:
+                return objects[i]
