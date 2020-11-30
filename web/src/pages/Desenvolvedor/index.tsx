b1a6aedb5b7bf550a450bc52627adf101b035ab3
Kaio César Viana <kaiocesarviana@localhost.localdomain>
2020-11-30 17:19:46 -0300

    page desenvolvedor finalizada

--- web/src/pages/Desenvolvedor/index.tsx
+++ web/src/pages/Desenvolvedor/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FiTwitter, FiInstagram, FiLinkedin } from 'react-icons/fi'
 import desenvolvedorImg from '../../images/desenvolvedor.svg'
 import './style.css'
 
@@ -14,7 +15,7 @@ function Desenvolvedor() {
                     </Link>
                 </header>
                 <main>
-                    <img src={desenvolvedorImg} alt="desenvolvedor"/>
+                    <img src={desenvolvedorImg} alt="desenvolvedor" />
                     <span>
                         Kaio César Viana de Oliveira Costa. Desenvolvedor RPA/Web, estudante de ciências da computação, data science, machinne learning e apaixonado por algoritmos.
                         Entusiasmado na área da programação  com conhecimentos nas liguagens e tecnologias Java, C#, JavaScript, Python e ReactJS.
@@ -23,6 +24,21 @@ function Desenvolvedor() {
                         "A arte imita a vida e a programação imita a arte"
                     </strong>
                 </main>
+                <footer>
+                    <span>Redes sociais</span>
+                    <span className="decoration"></span>
+                    <div>
+                        <a href="https://twitter.com/KaioVia25836155/" target="blanck">
+                            <FiTwitter size="40px"/>
+                        </a>
+                        <a href="https://www.instagram.com/kaiocesarviana/" target="blanck">
+                            <FiInstagram size="40px"/>
+                        </a>
+                        <a href="https://www.linkedin.com/in/kaio-c%C3%A9sar-viana-de-oliveira-costa-46aa66162/" target="blanck">
+                            <FiLinkedin size="40px"/>
+                        </a>
+                    </div>
+                </footer>
             </div>
         </>
     )
